b91ce9b4729e4ca2c825ab159d9d6475ef57e38c
André Santos <andre@dexdev.com.br>
2017-09-13 20:49:05 -0300

    Add autorization flow

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
-const API_URL = 'https://api.spotify.com/v1';
+const TOKEN_API = 'BQBEBLomZv_mb3bVeiiry22FT2QtnMh2dCC_im3MkYwTZLUH0zjDgwSJK_hITYG63KU10THlVGPUptzh-U-X-nAWvfQ4iffPqvy7GRnuV8SADZ-cp9ZUsGCFWj_ELpW_4XzuDVrb';
 
-export default API_URL;
+export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `'Bearer ${TOKEN_API}'`,
+  }
+};
